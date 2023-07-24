ddb42ca8b8fcdd1c431e924b130bc36417ce054e
martam00 <marta.michniacka1@gmail.com>
2023-07-24 22:45:55 +0200

    classes and objects

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String paws = this.numberOfPaws == 1 ? " paw" : " paws";
+        String preFur = hasFur ? "a fur." : "no fur.";
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + paws + " and " + preFur;
+    }
 }
---
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -0,0 +1,13 @@
+package com.epam.mjc.stage0;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog.getDescription());
+        System.out.println("*************************\n");
+
+        Bird bird = new Bird();
+        System.out.println(bird.getDescription());
+    }
+}
